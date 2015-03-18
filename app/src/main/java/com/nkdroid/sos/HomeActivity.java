d495cc27b0fe1fb110e044a1b5338c34e3b53530
Nirav <niravnvk@gmail.com>
2015-03-18 20:29:32 +0530

    abc

--- app/src/main/java/com/nkdroid/sos/HomeActivity.java
+++ app/src/main/java/com/nkdroid/sos/HomeActivity.java
@@ -1,22 +1,36 @@
 package com.nkdroid.sos;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 
 
 public class HomeActivity extends ActionBarActivity {
     private Toolbar toolbar;
     private Button btnLogout;
+    private GoogleCloudMessaging gcm;
+    private String regid;
+
+    private String PROJECT_NUMBER = "92884720384";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setToolbar();
         initView();
+        getRegId();
     }
 
     private void initView() {
@@ -26,7 +40,7 @@ public class HomeActivity extends ActionBarActivity {
             public void onClick(View v) {
                 //remove sharedpreference
                 PrefUtils.clearLogin(HomeActivity.this);
-                Intent intent=new Intent(HomeActivity.this,LoginActivity.class);
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -40,4 +54,60 @@ public class HomeActivity extends ActionBarActivity {
             setSupportActionBar(toolbar);
         }
     }
+
+    public void getRegId(){
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(HomeActivity.this);
+                    }
+                    regid = gcm.register(PROJECT_NUMBER);
+                    Log.e("GCM ID :", regid);
+
+                    if(regid==null || regid==""){
+                        AlertDialog.Builder alert = new AlertDialog.Builder(HomeActivity.this);
+                        alert.setTitle("Error");
+                        alert.setMessage("Internal Server Error");
+                        alert.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                getRegId();
+                                dialog.dismiss();
+                            }
+                        });
+                        alert.setNegativeButton("exit", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                finish();
+                            }
+                        });
+                        alert.show();
+                    } else {
+                        // Store GCM ID in sharedpreference
+                        SharedPreferences sharedPreferences=getSharedPreferences("GCM",MODE_PRIVATE);
+                        SharedPreferences.Editor editor=sharedPreferences.edit();
+                        editor.putString("GCM_ID",regid);
+                        editor.commit();
+
+                        // TODO further logic
+
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+
+            }
+        }.execute();
+
+    } // end of getRegId
 }
